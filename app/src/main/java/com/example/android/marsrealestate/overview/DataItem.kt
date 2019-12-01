526958ddec46f1959095080a35d3b9ea790ab98b
JimmyLau <kuopingl+1@gmail.com>
2019-12-01 20:23:31 +0800

    defined DataItem Seal Class

--- app/src/main/java/com/example/android/marsrealestate/overview/DataItem.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/DataItem.kt
@@ -1,2 +1,16 @@
 package com.example.android.marsrealestate.overview
 
+import com.example.android.marsrealestate.network.MarsProperty
+
+sealed class DataItem {
+    data class MarsPropertyItem (val marsProperty: MarsProperty): DataItem() {
+        override val id: String
+            get() = marsProperty.id
+    }
+    object Header: DataItem() {
+        override val id: String
+            get() = Long.MIN_VALUE.toString()
+    }
+
+    abstract val id: String
+}
